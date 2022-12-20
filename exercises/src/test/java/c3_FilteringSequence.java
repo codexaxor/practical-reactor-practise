cf8b514d10921871119a47564707a259ee888822
codexaxor <loicmeta@gmail.com>
2022-12-21 01:11:50 +0600

    c3_FilteringSequence - economical

--- exercises/src/test/java/c3_FilteringSequence.java
+++ exercises/src/test/java/c3_FilteringSequence.java
@@ -56,7 +56,7 @@ public class c3_FilteringSequence extends FilteringSequenceBase {
     @Test
     public void economical() {
         Flux<String> items = duplicated_records_service()
-                //todo: change this line only, use only one operator
+                .distinct()
                 ;
 
         StepVerifier.create(items)
