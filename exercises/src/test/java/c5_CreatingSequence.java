589d8cc6ea8cfe11d821da3af289f88dc937cbe4
Sajal <abuyousuf.sajal@cellosco.pe>
2022-12-26 20:56:10 +0600

    c5_CreatingSequence - trouble_maker

--- exercises/src/test/java/c5_CreatingSequence.java
+++ exercises/src/test/java/c5_CreatingSequence.java
@@ -153,7 +153,7 @@ public class c5_CreatingSequence {
      */
     @Test
     public void trouble_maker() {
-        Mono<String> trouble = null; //todo: change this line
+        Mono<String> trouble = Mono.error(new IllegalStateException());
 
         StepVerifier.create(trouble)
                 .expectError(IllegalStateException.class)
